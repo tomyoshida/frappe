bb0f9964cd33094a3fcad8fe3913a8f52827efa7
Tomohiro C. Yoshida <tomohiroyoshida.astro@gmail.com>
2026-01-19 16:54:06 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,5 +23,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx-rtd-theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+extensions = [
+    'sphinx_rtd_theme',
+]
