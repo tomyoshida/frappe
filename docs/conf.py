7badeed546967bdf422876e8dd2e1f8e2783bc78
Tomohiro C. Yoshida <tomohiroyoshida.astro@gmail.com>
2026-01-19 16:28:22 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'emban'
+project = 'frappe'
 copyright = '2025, Tomohiro C. Yoshida'
 author = 'Tomohiro C. Yoshida'
 
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
