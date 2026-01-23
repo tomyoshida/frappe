2d09338232fb0ff38d7e2d2b008e61dbf56b5cdb
Tomohiro C. Yoshida <tomohiroyoshida.astro@gmail.com>
2026-01-23 17:11:10 +0900

    Update __init__.py

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -1,8 +1,10 @@
 # Import key classes and functions for user access
-from .model import *
-from ._utilities import *
-from ._constants import *
-from .msdata import *
+
+
+#from .model import *
+#from ._utilities import *
+#from ._constants import *
+#from .msdata import *
 # from .prep import *
 
 # Version information
